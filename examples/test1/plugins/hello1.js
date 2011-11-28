7335f3a4cc04498698ab7fe928c935a1a515ba42
Craig Condon <craig@spiceapps.com>
2011-11-28 13:38:42 -0600

    updated example

--- examples/test1/plugins/hello1.js
+++ examples/test1/plugins/hello1.js
@@ -2,7 +2,9 @@ exports.require = {
 	hello2: 'hello2'
 };
 
-exports.plugin = function() {
+exports.plugin = function(ops, params) {
+	console.log(ops);
+	console.log(params);
 	return {
 		init: function() {
 			this.require.hello2.plugin.sayHello();
